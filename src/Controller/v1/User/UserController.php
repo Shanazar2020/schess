88ea52b73b0c392628bd7467da5263ad9e94284e
Shanazar <snizamaddinow@gmail.com>
2023-11-10 23:39:33 +0300

    user controller update.

--- src/Controller/v1/User/UserController.php
+++ src/Controller/v1/User/UserController.php
@@ -2,7 +2,22 @@
 
 namespace App\Controller\v1\User;
 
-class UserController
+use App\Controller\v1\MainController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends MainController
 {
+    /**
+     * @Route(path: '/user', name: 'app_user_index', methods: ['GET'])
+     */
+    public function index(Request $request): JsonResponse
+    {
+        return $this->json([
+            'name' => 'schess-service',
+            'path' => '/user'
+        ]);
+    }
 
 }
